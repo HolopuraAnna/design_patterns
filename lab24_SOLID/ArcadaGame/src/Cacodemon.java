5e935be2d56f5e11827c5b1cbc437ffea39b6480
Anna Holopura <aholopura@agroprosperis.com>
2026-03-22 22:04:14 +0200

    1.2. SOLID ArcadaGame

---
+++ lab24_SOLID/ArcadaGame/src/Cacodemon.java
@@ -0,0 +1,12 @@
+public class Cacodemon {
+
+    private final String level;
+
+    public Cacodemon(String level) {
+        this.level = level;
+    }
+
+    public void shoot() {
+        System.out.printf("Creature: %s, level %s, action: shoots\n", "Cacodemon", level);
+    }
+}
